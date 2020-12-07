515f01e59169f3afc9e27ea9a656f0e9d37b1cd8
Vadim <Shkuratov>
2020-12-07 12:57:38 +0300

    Add solution task 1 in branch task1

---
+++ Task_1_EPAM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_1_EPAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
