1f7e82151b1dd55928d58956aa76967e7720f19f
Vadim <Shkuratov>
2020-12-29 05:30:10 +0300

    Initial task3 branch

---
+++ Task_3_EPAM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_3_EPAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
